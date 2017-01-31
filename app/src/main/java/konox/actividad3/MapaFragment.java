e0ab5e2aea7cd96ddf47d97d08affc7b975b1051
konox95 <konox95@gmail.com>
2017-01-31 12:14:33 +0100

    Se muestra mapa

--- app/src/main/java/konox/actividad3/MapaFragment.java
+++ app/src/main/java/konox/actividad3/MapaFragment.java
@@ -6,16 +6,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 /**
  * Created by konox on 18/01/2017.
  */
 
-public class MapaFragment extends Fragment {
+public class MapaFragment extends SupportMapFragment {
+
+    public MapaFragment() {
+    }
+
+    public static MapaFragment newInstance() {
+        return new MapaFragment();
+    }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_mapa, container, false);
-        return v;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+
+        return root;
     }
 }
