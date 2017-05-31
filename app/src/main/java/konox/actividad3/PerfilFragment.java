4eadef0d7c738035d34823077089fdd687e3cf03
CharlyBiersack <carlos_lallama@hotmail.com>
2017-05-31 17:46:44 +0200

    Perfil casiterminado, cambiar contraseña y foto

--- app/src/main/java/konox/actividad3/PerfilFragment.java
+++ app/src/main/java/konox/actividad3/PerfilFragment.java
@@ -2,9 +2,11 @@ package konox.actividad3;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by konox on 18/01/2017.
@@ -12,9 +14,21 @@ import android.view.ViewGroup;
 
 public class PerfilFragment extends Fragment {
 
+    TextView tvNombrePerfil;
+    TextView tvEmailPErfil;
+    TextView tvContraseñaPerfil;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        tvNombrePerfil= (TextView) v.findViewById(R.id.tvNombreUsuarioPerfil);
+        tvEmailPErfil= (TextView) v.findViewById(R.id.tvEmaileUsuarioPerfil);
+        //tvContraseñaPerfil= (TextView) v.findViewById(R.id.tvEmaileUsuarioPerfil);
+
+        tvNombrePerfil.setText(DataHolder.instance.name);
+        tvEmailPErfil.setText(DataHolder.instance.email);
+
         return v;
     }
 }
